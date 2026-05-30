316e2bd48ae98ae723ab4e5f9575ecc14365b937
NoisyWater <kianhargreaves107@gmail.com>
2026-05-30 22:39:25 +0100

    enemy movement ai

--- AStepOverTheEdge/Assets/Scripts/Enemy.cs
+++ AStepOverTheEdge/Assets/Scripts/Enemy.cs
@@ -1,20 +1,50 @@
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+using System.Linq;
 
 public class Enemy : MonoBehaviour
 {
     public int health = 100;
-
     private Rigidbody rb;
+
+    private NavMeshAgent agent;
+
+    //AI settings
+    public int currentPointIndex = 0;
+    public Vector3 currentTarget;
+    public float positionThreshold;
+    public float idleTime = 5f;
+    public float attackDistance = 5f;
+    public float maxVisionDistance = 20f;
+    public float minChasingHealth = 30f;
+
+    public Transform[] patrolPoints;
+    private float idleTimeCounter;
+    private Transform playerTransform;
+    private bool canSeePlayer;
+    private Vector3 lastKnownPlayerPosition;
+
+    public enum State { Idle, Patrolling, Chasing, Attacking }
+    public State state = State.Idle;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        agent = GetComponent<NavMeshAgent>();
+        playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+
+        GameObject patrolPointParent = GameObject.FindWithTag("PatrolPoint");
+        patrolPoints = patrolPointParent.GetComponentsInChildren<Transform>().Where(t => t != patrolPointParent.transform).ToArray();                       
     }
     // mans shit wasnt working so did this 
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collided with: " + collision.gameObject.name);
         Debug.Log("Tag is: '" + collision.gameObject.tag + "'");
+
 
         bool isDamage = collision.gameObject.CompareTag("Damage");
         Debug.Log("CompareTag result: " + isDamage);
@@ -36,5 +66,133 @@ public class Enemy : MonoBehaviour
     void Die()
     {
         Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        LookForPlayer();
+
+        switch (state)
+        {
+            case State.Idle:
+                Idle();
+                break;
+            case State.Patrolling:
+                Patrolling();
+                break;
+            case State.Attacking:
+                Attacking();
+                break;
+            case State.Chasing:
+                Chasing();
+                break;  
+        }
+
+        rb.linearVelocity = Vector3.zero;
+
+        
+        SetLastKnownPlayerPosition();
+    }
+
+    private void LookForPlayer()
+    {
+        Vector3 directionToPlayer = playerTransform.position - transform.position;
+
+        if (Physics.Raycast(transform.position, directionToPlayer, out RaycastHit hit, maxVisionDistance))
+        {
+            canSeePlayer = hit.transform == playerTransform;
+
+            if (canSeePlayer && state != State.Attacking)
+            {
+                state = State.Chasing;
+            }
+        }
+    }
+
+    private void Idle()
+    {
+        agent.ResetPath();
+
+        idleTimeCounter -= Time.deltaTime;
+
+        if (idleTimeCounter < 0)
+        {
+            state = State.Patrolling;
+            idleTimeCounter = idleTime;
+        }
+    }
+
+    private void Patrolling()
+    {
+        if (Vector3.Distance(currentTarget, transform.position) < positionThreshold)
+        {
+            float chance = Random.Range(0, 100);
+
+            if (chance < 10)
+            {
+                state = State.Idle;
+                return;
+            }
+            currentPointIndex++;
+            currentTarget = patrolPoints[currentPointIndex % patrolPoints.Length].position;
+        }
+        else
+        {
+            agent.SetDestination(currentTarget);
+        }
+    }
+
+    private void Attacking()
+    {
+        idleTimeCounter = idleTime; 
+        agent.ResetPath();
+
+        if (Vector3.Distance(transform.position, playerTransform.position) > attackDistance || !canSeePlayer)
+        {
+            if (health < minChasingHealth)
+            {
+                state = State.Patrolling;
+            }
+            else
+            {
+                state = State.Chasing;
+            }
+        }
+    }
+
+    private void Chasing()
+    {
+        idleTimeCounter = idleTime;
+        agent.SetDestination(lastKnownPlayerPosition);
+
+        if (health < minChasingHealth)
+        {
+            state = State.Patrolling;
+        }
+
+        else if (Vector3.Distance(transform.position, playerTransform.position) <= attackDistance && canSeePlayer)
+        {
+            state = State.Attacking;
+        }
+
+        else if (Vector3.Distance(transform.position, playerTransform.position) > maxVisionDistance)
+        {
+            state = State.Patrolling;
+        }
+
+
+        else if (Vector3.Distance(transform.position, playerTransform.position) < positionThreshold && !canSeePlayer)
+        {
+            state = State.Patrolling;
+        }
+        
+    }
+
+    private void SetLastKnownPlayerPosition()
+    {
+        if (canSeePlayer)
+        {
+            lastKnownPlayerPosition = playerTransform.position;
+        }
     }
 }
